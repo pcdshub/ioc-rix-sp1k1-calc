be5919d8840f3fe8f221a71384ca4c5e41232c80
Zachary Lentz <zlentz@slac.stanford.edu>
2021-05-26 13:22:37 -0700

    MNT: typo in st.cmd

--- st.cmd
+++ st.cmd
@@ -2,4 +2,4 @@
 source /reg/g/pcds/pyps/conda/py36/etc/profile.d/conda.sh
 conda activate pcds-4.0.1
 
-python ioc-rix-sp1k1-calc/__main__.py --list-pvs | tee /cds/data/iocData/ioc-rix-sp1k1-calc/iocInfo/ioc.log
+python ioc_rix_sp1k1_calc/__main__.py --list-pvs | tee /cds/data/iocData/ioc-rix-sp1k1-calc/iocInfo/ioc.log
